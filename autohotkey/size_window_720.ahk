339016086b6aff901cd4aed97cf7d8059a4b470c
on-three <on.three.email@gmail.com>
2014-09-24 22:45:18 -0700

    Added some 720p support scripts.

---
+++ autohotkey/size_window_720.ahk
@@ -0,0 +1,28 @@
+/*
+if 0 != 4
+{
+  MsgBox incorrect number of arguments exiting
+  ExitApp
+}
+*/
+/*
+SysGet, MonitorCount, MonitorCount
+SysGet, MonitorPrimary, MonitorPrimary
+MsgBox, Monitor Count:`t%MonitorCount%`nPrimary Monitor:`t%MonitorPrimary%
+Loop, %MonitorCount%
+{
+    SysGet, MonitorName, MonitorName, %A_Index%
+    SysGet, Monitor, Monitor, %A_Index%
+    SysGet, MonitorWorkArea, MonitorWorkArea, %A_Index%
+    MsgBox, Monitor:`t#%A_Index%`nName:`t%MonitorName%`nLeft:`t%MonitorLeft% (%MonitorWorkAreaLeft% work)`nTop:`t%MonitorTop% (%MonitorWorkAreaTop% work)`nRight:`t%MonitorRight% (%MonitorWorkAreaRight% work)`nBottom:`t%MonitorBottom% (%MonitorWorkAreaBottom% work)
+}
+*/
+;setformat,float, 0.2
+x:=(480.0*0.66)
+y:=(2.0*0.66)
+w:=(1290.0*0.66)
+h:=(766*0.66)
+CoordMode, Mouse, Screen
+WinMove,SlingPlayer,,x,y,w,h 
+
+Return
---
+++ windows/capture_window_480.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+"""
+capture_window_480.py
+
+"""
+import cygwinreg
+import argparse
+
+def set_registry(top, left, width, height):
+  '''set screen capture recorder registry settings
+  to desired pos and dimensions
+  '''
+  cap = cygwinreg.OpenKey(
+    cygwinreg.HKEY_CURRENT_USER,
+    "Software\\screen-capture-recorder", 0, cygwinreg.KEY_ALL_ACCESS
+    )
+  values = ['capture_height', 'capture_width', 'start_x', 'start_y',]
+  #for value in values:  
+  #  v, t = cygwinreg.QueryValueEx(cap, value)
+  #  print "{v} is {value}".format(v=v, value=value)
+  set_value(cap, 'start_x', left)
+  set_value(cap, 'start_y', top)
+  set_value(cap, 'capture_width', width)
+  set_value(cap, 'capture_height', height)
+  
+def set_value(key, subkey, value):
+  #first echo the current value
+  v, t = cygwinreg.QueryValueEx(key, subkey)
+  print "{subkey} initial value {v} and type {t}".format(subkey=subkey, v=v, t=str(t))
+  #cygwinreg.SetValue(key, subkey, t, cygwinreg.REG_SZ(str(value)))
+  #cygwinreg.SetValue(key, subkey, cygwinreg.REG_SZ, str(value))
+  cygwinreg.SetValueEx(key, subkey, 0, t, value)
+
+  v, t = cygwinreg.QueryValueEx(key, subkey)
+  print "{subkey} final value {v}".format(subkey=subkey, v=v)
+  
+  
+def main():
+  #parser = argparse.ArgumentParser(description='Exercise bing translation api.')
+  #parser.add_argument('text', help='Input text to translate.', type=str)
+  #args = parser.parse_args()
+  #text = args.text.decode('utf-8')
+  #translation = translate(text)
+  #print(translation)#.encode('utf-8'))
+  set_registry(20, 1037, 848, 480)
+
+if __name__ == "__main__":
+  main()
+
